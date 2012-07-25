15a4031afb220be69ecbd41f119410f892305ac2
Gustavo Hingel Morada <gusmorada@gmail.com>
2012-07-25 01:29:03 -0300

    Acerta css de relatorio

--- apps/Manager/modules/Index/templates/reportSuccess.php
+++ apps/Manager/modules/Index/templates/reportSuccess.php
@@ -1,77 +1,78 @@
-<div class="export">
-    <span class="largeButton"><a href="<?php echo url_for('Index/reportExcel'); ?>">Exportar para Excel</a></span>
-</div>
-
-<table>
-    <thead>
-        <tr>
-            <th>Código</th>
-            <th>Disciplina</th>
-            <th>Turma</th>
-            <th>Cursos</th>
-            <th>Módulo</th>
-            <th>Segunda</th>
-            <th>Terça</th>
-            <th>Quarta</th>
-            <th>Quinta</th>
-            <th>Sexta</th>
-            <th>Sábado</th>
-            <th>Professor</th>
-        </tr>
-    </thead>
-    <tbody>
-<?php
-    foreach($crowds as $crowd)
-    {
-        $roomCrowdDateTimes = Doctrine::getTable('RoomCrowdDatetime')->findByCrowCdKey($crowd->getCrowCdKey());
-        
-        $monday = "";
-        $tuesday = "";
-        $wednesday = "";
-        $thursday = "";
-        $friday = "";
-        $saturday = "";
-        
-        foreach($roomCrowdDateTimes as $roomCrowdDateTime)
-        {
-            switch($roomCrowdDateTime->getRocdNbWeekday())
+<div class="row">
+    <div class="span12">
+        <h1 class="title">Relatório <a href="<?php echo url_for('Index/reportExcel'); ?>" class="btn btn-inverse" title="Exportar para Excel"><i class="icon-download-alt icon-white"></i> Exportar para Excel</a></h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Código</th>
+                    <th>Disciplina</th>
+                    <th>Turma</th>
+                    <th>Cursos</th>
+                    <th>Módulo</th>
+                    <th>Segunda</th>
+                    <th>Terça</th>
+                    <th>Quarta</th>
+                    <th>Quinta</th>
+                    <th>Sexta</th>
+                    <th>Sábado</th>
+                    <th>Professor</th>
+                </tr>
+            </thead>
+            <tbody>
+        <?php
+            foreach($crowds as $crowd)
             {
-                case 1:
-                    $monday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
-                    break;
-                case 2:
-                    $tuesday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
-                    break;
-                case 3:
-                    $wednesday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
-                    break;
-                case 4:
-                    $thursday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
-                    break;
-                case 5:
-                    $friday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
-                    break;
-                default :
-                    $saturday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
+                $roomCrowdDateTimes = Doctrine::getTable('RoomCrowdDatetime')->findByCrowCdKey($crowd->getCrowCdKey());
+                
+                $monday = "";
+                $tuesday = "";
+                $wednesday = "";
+                $thursday = "";
+                $friday = "";
+                $saturday = "";
+                
+                foreach($roomCrowdDateTimes as $roomCrowdDateTime)
+                {
+                    switch($roomCrowdDateTime->getRocdNbWeekday())
+                    {
+                        case 1:
+                            $monday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
+                            break;
+                        case 2:
+                            $tuesday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
+                            break;
+                        case 3:
+                            $wednesday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
+                            break;
+                        case 4:
+                            $thursday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
+                            break;
+                        case 5:
+                            $friday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
+                            break;
+                        default :
+                            $saturday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
+                    }
+                }
+            ?>
+            <tr>
+                <td><?php echo $crowd->Subject->getSubjNmCode(); ?></td>
+                <td><?php echo $crowd->Subject->getSubjNmName(); ?></td>
+                <td><?php echo $crowd ?></td>
+                <td><?php echo $crowd->Subject->Course; ?></td>
+                <td><?php echo $crowd->getCrowNbModule(); ?></td>
+                <td><?php echo $monday; ?></td>
+                <td><?php echo $tuesday; ?></td>
+                <td><?php echo $wednesday; ?></td>
+                <td><?php echo $thursday; ?></td>
+                <td><?php echo $friday; ?></td>
+                <td><?php echo $saturday; ?></td>
+                <td><?php echo $crowd->Teacher; ?></td>
+            </tr>
+            <?php
             }
-        }
-    ?>
-    <tr>
-        <td><?php echo $crowd->Subject->getSubjNmCode(); ?></td>
-        <td><?php echo $crowd->Subject->getSubjNmName(); ?></td>
-        <td><?php echo $crowd ?></td>
-        <td><?php echo $crowd->Subject->Course; ?></td>
-        <td><?php echo $crowd->getCrowNbModule(); ?></td>
-        <td><?php echo $monday; ?></td>
-        <td><?php echo $tuesday; ?></td>
-        <td><?php echo $wednesday; ?></td>
-        <td><?php echo $thursday; ?></td>
-        <td><?php echo $friday; ?></td>
-        <td><?php echo $saturday; ?></td>
-        <td><?php echo $crowd->Teacher; ?></td>
-    </tr>
-    <?php
-    }
-?>
-    </tbody>
-</table>
+        ?>
+            </tbody>
+        </table>
+    </div><!--end span12-->
+</div><!--end row-->
