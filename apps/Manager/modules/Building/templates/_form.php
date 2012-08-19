1773c8fd6007fad5cb9985725ede6e256f60c681
Gustavo Hingel Morada <gusmorada@gmail.com>
2012-08-19 17:49:31 -0300

    Coloca a tela de cadastro e edição de prédio no novo layout

--- apps/Manager/modules/Building/templates/_form.php
+++ apps/Manager/modules/Building/templates/_form.php
@@ -1,32 +1,30 @@
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
 
-<form action="<?php echo url_for('Building/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?buil_cd_key='.$form->getObject()->getBuilCdKey() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
-<?php if (!$form->getObject()->isNew()): ?>
-<input type="hidden" name="sf_method" value="put" />
-<?php endif; ?>
-  <table>
-    <tfoot>
-      <tr>
-          <td>
-          <?php echo $form->renderHiddenFields(false) ?>
-        <td>
-            <span class="buttons">
-                <span class="back"><a class="button" href="<?php echo url_for('Building/index') ?>"></a></span>
-                <span class="save"><input class="button" type="image" value="" /></span>
-            </span>
-        </td>
-      </tr>
-    </tfoot>
-    <tbody>
-      <?php echo $form->renderGlobalErrors() ?>
-      <tr>
-        <th><?php echo $form['buil_nm_name']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['buil_nm_name']->renderError() ?>
-          <?php echo $form['buil_nm_name'] ?>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</form>
+
+<div class="row">
+  <div class="span12">
+    <h1 class="title"><?php echo ($form->getObject()->isNew() ? 'Cadastrar novo prédio': 'Editar prédio')?></h1>
+    <form class="form-horizontal well form_inside" action="<?php echo url_for('Building/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?buil_cd_key='.$form->getObject()->getBuilCdKey() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
+    <?php if (!$form->getObject()->isNew()): ?>
+    <input type="hidden" name="sf_method" value="put" />
+    <?php endif; ?>
+    <?php echo $form->renderHiddenFields(false) ?>
+    <?php echo $form->renderGlobalErrors() ?>
+      <legend><?php echo ($form->getObject()->isNew() ? 'Cadastro': 'Edição')?></legend>
+      <fieldset>
+        <div class="control-group">
+          <label class="control-label" for="input01"><?php echo $form['buil_nm_name']->renderLabelName() ?></label>
+          <div class="controls">
+            <?php echo $form['buil_nm_name']->renderError() ?>
+            <?php echo $form['buil_nm_name']->render(array('class'=>'span3')) ?>            
+          </div>
+        </div>
+        <div class="form-actions">
+          <a class="btn" href="<?php echo url_for('Building/index') ?>">« Voltar</a>
+          <button class="btn btn-success" type="submit">Salvar</button>
+        </div>
+      </fieldset>
+    </form>
+  </div><!--end span12-->
+</div><!--end row-->
--- apps/Manager/modules/Building/templates/newSuccess.php
+++ apps/Manager/modules/Building/templates/newSuccess.php
@@ -1,5 +1,4 @@
 
-    <h1>Novo Prédio</h1>
 
     <?php include_partial('form', array('form' => $form)) ?>
 
