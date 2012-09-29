e14c64e582eb726b5caec5462201d0755b8da985
Gustavo Hingel Morada <gusmorada@gmail.com>
2012-09-29 01:06:34 -0300

    Retira h1 de login

--- apps/Manager/modules/sfGuardAuth/templates/signinSuccess.php
+++ apps/Manager/modules/sfGuardAuth/templates/signinSuccess.php
@@ -1,7 +1,4 @@
 <?php use_helper('I18N') ?>
 <div class="loginContent">
-
-    <h1><?php echo __('Login', null, 'sf_guard') ?></h1>
-
     <?php echo get_partial('sfGuardAuth/signin_form', array('form' => $form)) ?>
 </div>
