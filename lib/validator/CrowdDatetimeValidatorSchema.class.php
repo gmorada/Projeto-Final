8b2a705f91aa67ba5c3e507aceb55bc2fcc02088
Gustavo Hingel Morada <gusmorada@gmail.com>
2012-09-30 20:58:58 -0300

    Adiciona validação de professor na alocação do horário

--- lib/validator/CrowdDatetimeValidatorSchema.class.php
+++ lib/validator/CrowdDatetimeValidatorSchema.class.php
@@ -14,7 +14,7 @@ class CrowdDatetimeValidatorSchema extends sfValidatorBase
           throw new sfValidatorError($this, 'invalid');
         }
         
-        $crowd = $values['crow_cd_key'];
+        $crowd_id = $values['crow_cd_key'];
         
         $startTime = false;
         $time = $values['rocd_dt_start_time'];        
@@ -36,7 +36,7 @@ class CrowdDatetimeValidatorSchema extends sfValidatorBase
         
         if(($startTime) && ($finalTime))
         {
-            $roomCrowdDateTimes = Doctrine::getTable('RoomCrowdDatetime')->findByCrowCdKey($crowd);
+            $roomCrowdDateTimes = Doctrine::getTable('RoomCrowdDatetime')->findByCrowCdKey($crowd_id);
         
             foreach($roomCrowdDateTimes as $roomCrowdDateTime)
             {
@@ -61,8 +61,52 @@ class CrowdDatetimeValidatorSchema extends sfValidatorBase
                 }
 
             }
-        }
+            
+            $crowd = Doctrine::getTable('Crowd')->find($crowd_id);
+            $teacher = $crowd->getTeacCdKey();
+            
+            if($teacher)
+            {
+                $crowds = Doctrine::getTable('Crowd')
+                    ->createQuery('c')
+                    ->where('c.teac_cd_key = ?',$teacher)
+                    ->andWhere('c.crow_cd_key <> ?', $crowd_id)
+                    ->execute();
+                
+                if($crowds)
+                {
+                    foreach($crowds as $actualCrowd)
+                    {                
+                        $actualRoomCrowdDateTimes = Doctrine::getTable('RoomCrowdDatetime')->findByCrowCdKey($actualCrowd->getCrowCdKey());
 
+                        foreach($actualRoomCrowdDateTimes as $roomCrowdDateTime)
+                        {
+                            if($roomCrowdDateTime->getRocdNbWeekday() == $values['rocd_nb_weekday'])
+                            {
+                                if(($roomCrowdDateTime->getRocdDtStartTime() <= $startTime) && ($roomCrowdDateTime->getRocdDtFinalTime() > $startTime))
+                                {
+                                    $errorSchema->addError(new sfValidatorError($this, 'Esse horário não pode ser alocado para essa turma, o professor dessa turma já está dando outra aula'), 'rocd_nb_weekday');
+                                }
+                                else if(($roomCrowdDateTime->getRocdDtFinalTime() >= $finalTime) && ($roomCrowdDateTime->getRocdDtStartTime() < $finalTime))
+                                {
+                                    $errorSchema->addError(new sfValidatorError($this, 'Esse horário não pode ser alocado para essa turma, o professor dessa turma já está dando outra aula'), 'rocd_nb_weekday');
+                                }
+                                else if(($startTime <= $roomCrowdDateTime->getRocdDtStartTime()) && ($finalTime > $roomCrowdDateTime->getRocdDtStartTime()))
+                                {
+                                    $errorSchema->addError(new sfValidatorError($this, 'Esse horário não pode ser alocado para essa turma, o professor dessa turma já está dando outra aula'), 'rocd_nb_weekday');
+                                }
+                                else if(($finalTime >= $roomCrowdDateTime->getRocdDtFinalTime()) && ($startTime < $roomCrowdDateTime->getRocdDtFinalTime()))
+                                {
+                                    $errorSchema->addError(new sfValidatorError($this, 'Esse horário não pode ser alocado para essa turma, o professor dessa turma já está dando outra aula'), 'rocd_nb_weekday');
+                                }
+                            }
+                        }
+                    }            
+                }
+            }
+        }
+        
+        
         if (count($errorSchema))
         {
             throw new sfValidatorErrorSchema($this, $errorSchema);
