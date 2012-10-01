1bdefd3a6a6a5c7f050846b2261e27511f74e5e4
Gustavo Hingel Morada <gusmorada@gmail.com>
2012-09-30 22:00:50 -0300

    Altera mensagem de validação de string muito grande

--- apps/Manager/config/ManagerConfiguration.class.php
+++ apps/Manager/config/ManagerConfiguration.class.php
@@ -5,5 +5,7 @@ class ManagerConfiguration extends sfApplicationConfiguration
   public function configure()
   {
       sfValidatorBase::setDefaultMessage('required', 'Obrigatório');
+      sfValidatorBase::setDefaultMessage('invalid', 'Valor inválido');
+      sfValidatorBase::setDefaultMessage('max_length', '"%value%" é muito grande (%max_length% caracateres no máximo).');      
   }
 }
