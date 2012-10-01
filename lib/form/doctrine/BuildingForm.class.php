7a82a36b2bea8dd6d6d245978e2555ca3121d0cd
Eduardo Zanuto <duduzanuto@hotmail.com>
2012-09-30 22:57:14 -0300

    Validacoes de unicidade de predio, sala e professor

--- lib/form/doctrine/BuildingForm.class.php
+++ lib/form/doctrine/BuildingForm.class.php
@@ -15,5 +15,30 @@ class BuildingForm extends BaseBuildingForm
       $this->widgetSchema->setLabels(array(
           'buil_nm_name'    => 'Prédio'
         ));
+      
+      $this->validatorSchema->setPostValidator(
+        new sfValidatorCallback(array('callback' => array($this, 'checkCode')))
+    );
+  }
+
+  public function checkCode($validator, $values)
+  {
+        $errorSchema = new sfValidatorErrorSchema($validator);
+        $name = $values['buil_nm_name'];
+        if ($name != "")
+        {
+            $buildings = Doctrine::getTable('Building')->findByBuilNmName($name);
+            foreach ($buildings as $building)
+            {
+                if($building->getBuilCdKey() != $values['buil_cd_key'])
+                    $errorSchema->addError(new sfValidatorError($validator, 'Já existe um Prédio com esse nome'), 'buil_nm_name');
+            }
+        }
+
+        if (count($errorSchema))
+        {
+            throw new sfValidatorErrorSchema($validator, $errorSchema);
+        }
+        return $values;
   }
 }
--- lib/form/doctrine/TeacherForm.class.php
+++ lib/form/doctrine/TeacherForm.class.php
@@ -16,5 +16,29 @@ class TeacherForm extends BaseTeacherForm
           'teac_nm_name'    => 'Nome',
           'teac_nm_email'    => 'Email'
       ));
+    $this->validatorSchema->setPostValidator(
+        new sfValidatorCallback(array('callback' => array($this, 'checkCode')))
+    );
+  }
+
+  public function checkCode($validator, $values)
+  {
+        $errorSchema = new sfValidatorErrorSchema($validator);
+        $name = $values['teac_nm_name'];
+        if ($name != "")
+        {
+            $teachers = Doctrine::getTable('Teacher')->findByTeacNmName($name);
+            foreach ($teachers as $teacher)
+            {
+                if($teacher->getTeacCdKey() != $values['teac_cd_key'])
+                    $errorSchema->addError(new sfValidatorError($validator, 'Já existe um Professor com esse nome'), 'buil_nm_name');
+            }
+        }
+
+        if (count($errorSchema))
+        {
+            throw new sfValidatorErrorSchema($validator, $errorSchema);
+        }
+        return $values;
   }
 }
