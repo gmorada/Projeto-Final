1d909338c6a60a969b1b427ffb1f1a4813713739
Gustavo Hingel Morada <gusmorada@gmail.com>
2012-11-04 23:28:05 -0200

    Acerta validador

--- lib/validator/CrowdValidatorSchema.class.php
+++ lib/validator/CrowdValidatorSchema.class.php
@@ -3,8 +3,8 @@
 class CrowdValidatorSchema extends sfValidatorBase
 {
     protected function configure($options = array(), $messages = array())
-    {        
-        
+    {
+
     }
 
     protected function doClean($values)
@@ -13,32 +13,35 @@ class CrowdValidatorSchema extends sfValidatorBase
         {
           throw new sfValidatorError($this, 'invalid');
         }
-        
+
         $crow_id = $values['crow_cd_key'];
-        
-        $modulo = $values['crow_nb_module'];
-        
-        $crowdDatetimes = Doctrine::getTable('RoomCrowdDatetime')->findByCrowCdKey($crow_id);
-        
+
         $errorSchema = new sfValidatorErrorSchema($this);
-        
-        if($crowdDatetimes)
+
+        if($crow_id != null)
         {
-            foreach($crowdDatetimes as $actualCrowdDatetime)
+            $modulo = $values['crow_nb_module'];
+
+            $crowdDatetimes = Doctrine::getTable('RoomCrowdDatetime')->findByCrowCdKey($crow_id);
+
+            if($crowdDatetimes)
             {
-                $room = Doctrine::getTable('Room')->findOneByRoomCdKey($actualCrowdDatetime->getRoomCdKey());
-                
-                if($room->getRoomNbVagas() < $modulo)
-                    $errorSchema->addError(new sfValidatorError($this, 'O modulo da turma é maior do que o número de vagas das salas alocadas para essa turma'), 'crow_nb_module');
+                foreach($crowdDatetimes as $actualCrowdDatetime)
+                {
+                    $room = Doctrine::getTable('Room')->findOneByRoomCdKey($actualCrowdDatetime->getRoomCdKey());
+
+                    if($room->getRoomNbVagas() < $modulo)
+                        $errorSchema->addError(new sfValidatorError($this, 'O modulo da turma é maior do que o número de vagas das salas alocadas para essa turma'), 'crow_nb_module');
+                }
+
             }
-            
         }
-        
+
         if (count($errorSchema))
         {
             throw new sfValidatorErrorSchema($this, $errorSchema);
         }
-        
+
         return $values;
     }
 }
