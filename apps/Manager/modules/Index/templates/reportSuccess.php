b0e7165de9bdbeb56e1b13720592d13ea9959ff1
Gustavo Hingel Morada <gusmorada@gmail.com>
2012-11-12 01:06:19 -0200

    Acerta relatorio para exibir turmas juntadas

--- apps/Manager/modules/Index/templates/reportSuccess.php
+++ apps/Manager/modules/Index/templates/reportSuccess.php
@@ -24,6 +24,7 @@
                     <th>Sexta</th>
                     <th>Sábado</th>
                     <th>Professor</th>
+                    <th>Juntada</th>
                 </tr>
             </thead>
             <tbody>
@@ -62,21 +63,71 @@
                             $saturday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
                     }
                 }
-            ?>
-            <tr>
-                <td><?php echo $crowd->Subject->getSubjNmCode(); ?></td>
-                <td><?php echo $crowd->Subject->getSubjNmName(); ?></td>
-                <td><?php echo $crowd ?></td>
-                <td><?php echo $crowd->Subject->Course; ?></td>
-                <td><?php echo $crowd->getCrowNbModule(); ?></td>
-                <td><?php echo $monday; ?></td>
-                <td><?php echo $tuesday; ?></td>
-                <td><?php echo $wednesday; ?></td>
-                <td><?php echo $thursday; ?></td>
-                <td><?php echo $friday; ?></td>
-                <td><?php echo $saturday; ?></td>
-                <td><?php echo $crowd->Teacher; ?></td>
-            </tr>
+                $parent = Doctrine::getTable('Crowd')->findOneByCrowCdParent($crowd->getCrowCdKey());
+                if($parent)
+                {
+                    $roomCrowdDateTimes = Doctrine::getTable('RoomCrowdDatetime')->findByCrowCdKey($parent->getCrowCdKey());
+                
+                    $monday = "";
+                    $tuesday = "";
+                    $wednesday = "";
+                    $thursday = "";
+                    $friday = "";
+                    $saturday = "";
+
+                    foreach($roomCrowdDateTimes as $roomCrowdDateTime)
+                    {
+                        switch($roomCrowdDateTime->getRocdNbWeekday())
+                        {
+                            case 1:
+                                $monday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
+                                break;
+                            case 2:
+                                $tuesday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
+                                break;
+                            case 3:
+                                $wednesday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
+                                break;
+                            case 4:
+                                $thursday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
+                                break;
+                            case 5:
+                                $friday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
+                                break;
+                            default :
+                                $saturday .= $roomCrowdDateTime->getDetailedInformation()."<br>";
+                        }
+                    }
+                }                
+                $son  = $crowd->getCrowCdParent();
+                if($son)
+                {
+                    $son = Doctrine::getTable('Crowd')->findOneByCrowCdKey($son);
+                    $parent = $son;
+                }
+                ?>
+                <tr>
+                    <td><?php echo $crowd->Subject->getSubjNmCode(); ?></td>
+                    <td><?php echo $crowd->Subject->getSubjNmName(); ?></td>
+                    <td><?php echo $crowd ?></td>
+                    <td><?php echo $crowd->Subject->Course; ?></td>
+                    <td><?php echo $crowd->getCrowNbModule(); ?></td>
+                    <td><?php echo $monday; ?></td>
+                    <td><?php echo $tuesday; ?></td>
+                    <td><?php echo $wednesday; ?></td>
+                    <td><?php echo $thursday; ?></td>
+                    <td><?php echo $friday; ?></td>
+                    <td><?php echo $saturday; ?></td>
+                    <td><?php echo $crowd->Teacher; ?></td>
+                    <td>
+                        <?php
+                        if($parent)
+                        {
+                            echo $parent->getCrowdName();
+                        }
+                        ?>
+                    </td>
+                </tr>
             <?php
             }
         ?>
